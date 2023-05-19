d2d0b7a586e07b58ffc98cd3dd7e054530f462f8
codyallison <cody.allison26@gmail.com>
2023-05-19 09:35:56 -0500

    added comments to app.py, cleaned up unused cells notebook, created readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sqlalchemy-challenge
+
+Hello!
+
+the following repo contains files for Washington University bootcamp module 10 assignment. 
+There are two ways to interact with the data and files provided. 
+
+1. You can open the jupyter notebook file and run the cells to see analysis and plots of the precipitation data of the latest 12 months available. 
+this file is located "SurfsUp->climate.ipynb"
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -35,6 +35,7 @@ year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
 #################################################
 @app.route("/")
 def home():
+    #list of available routes
     return (f"Welcome to the Hawaii Climate API homepage!<br/>"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
@@ -47,37 +48,45 @@ def home():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-        
+    
+    #query measurement table for date and precipitation data for last 12 available month
     results = session.query(Measurement.date, Measurement.prcp).\
     filter(Measurement.date >=year_ago).\
     filter(Measurement.date <='2017-08-23').all()
     
     session.close()
     
+    #add results to dictionary
     precipitation_data={}
     for row in results:
         precipitation_data[row.date] = row.prcp
-        
+    
+    #return dict in JSON
     return jsonify(precipitation_data)
 
 @app.route("/api/v1.0/stations")
 def stations():
     
+    #query station table for station names
     results = session.query(Station.name).all()
     
     session.close()
     
+    #convert into normal list
     all_stations = list(np.ravel(results))
     
+    #return station names in JSON
     return jsonify(all_stations)
         
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     
+    #determine most active station by count of measurements
     most_active = session.query(Measurement.station).group_by(Measurement.station).\
     order_by(func.count(Measurement.station).desc()).first()
     
+    #query most active station for last 12 available months
     results = session.query(Measurement.date,Measurement.tobs).\
     filter(Measurement.station == most_active.station).\
     filter(Measurement.date >= year_ago).\
@@ -85,10 +94,13 @@ def tobs():
     
     session.close()
     
+    #create list of dictionaries containing most active station precipitation data
     results_data = []
     for row in results:
         data_dict = {row.date:row.tobs}
         results_data.append(data_dict)
+    
+    #return list  of measurements in JSON
     return jsonify(results_data)
         
         
@@ -96,11 +108,15 @@ def tobs():
         
 @app.route("/api/v1.0/<start>")
 def start(start):
+    
+    #create list of data needed for date analysis: Date, Min temp, Max temp, avg temp
     sel = [Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
     
+    #query measurement table with SEL data with start date as query parameter
     results = session.query(*sel).\
     filter(Measurement.date >= start).group_by(Measurement.date).all()
     
+    #create list of dictionaries. SEL data for each day
     temp_data = []
     for row in results:
         temp_dict = {}
@@ -113,11 +129,15 @@ def start(start):
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start,end):
+    
+    #create list of data needed for date analysis: Date, Min temp, Max temp, avg temp
     sel = [Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)]
     
+    #query measurement table with SEL data with start date and end date as query parameters
     results = session.query(*sel).\
     filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all()
     
+    #create list of dictionaries. SEL data for each day
     temp_data = []
     for row in results:
         temp_dict = {}
@@ -127,6 +147,7 @@ def start_end(start,end):
         temp_dict["AVG"] = row[3]
         temp_data.append(temp_dict)
     return jsonify(temp_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
